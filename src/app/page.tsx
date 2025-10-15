06b88a9f356580ef3e28a21d98edb52b71ae0acf
LucasEutacio <lucas123@gmail.com>
2025-10-15 08:29:54 -0300

    feat: book item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Header from "@/components/header/header";
+import { Avatar } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { AvatarImage } from "@radix-ui/react-avatar";
 import { Icon, Search, SearchIcon } from "lucide-react";
 import Image from "next/image";
 
@@ -26,10 +28,22 @@ export default function Home() {
         </div>
         <div>
           <Card className="mt-6">
-            <CardContent className="flex">
+            <CardContent className="flex justify-between p-0">
               <div className="flex flex-col gap-2 py-5 pl-5">
-                <Badge>Confirmado</Badge>
-                <h3>corte de cabelo</h3>
+                <Badge className="w-fit">Confirmado</Badge>
+                <h3 className="font-bold">corte de cabelo</h3>
+              <div className="flex items-center gap-2">
+                 <Avatar className="h-6 w-6">
+                   <AvatarImage src="../../public/logo.png"/>
+                 </Avatar>
+                  <p>Barbearia fullStack week</p>
+              </div>
+              </div>
+
+              <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid ">
+                  <p className="text-sm ">Agosto</p>
+                  <p className="text-2xl font-semibold">05</p>
+                  <p className="text-sm">20:00</p>
               </div>
             </CardContent>
           </Card>
